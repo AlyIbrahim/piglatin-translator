27c81284ed82d8bf6c7f2d137624c05708be4b12
AlyIbrahim <aly.ibrahim@gmail.com>
2020-10-13 21:11:16 +0000

    Initial Commit

---
+++ src/test/java/com/redhat/demo/NativePiglatinResourceIT.java
@@ -0,0 +1,9 @@
+package com.redhat.demo;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativePiglatinResourceIT extends PiglatinResourceTest {
+
+    // Execute the same tests but in native mode.
+}
